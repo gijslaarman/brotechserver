b072791afc7b4b5388a50c999c2bd56f8ac0da03
gijslaarman <gijslaarman@live.nl>
2020-03-20 10:01:23 +0100

    Updated files

--- routes/carousel/index.js
+++ routes/carousel/index.js
@@ -1,39 +1,59 @@
 const express = require('express')
 const router = express.Router({ mergeParams: true })
+const multer = require('multer')
+const upload = multer()
+const fs = require('fs')
+// const filterImages = (query, array) => {
+//     if (!query) {
+//         return false
+//     }
 
-const filterImages = (query, array) => {
-    if (!query) {
-        return false
-    }
+//     if (typeof query.i === 'string') {
+//         return [array[query.i]]
+//     } else {
+//         let filteredArray = []
+//         query.i.forEach((no, index) => {
+//             let object = array[index]
+            
+            
+//             if (index > 0) {
+//                 // Does this image need a previous button?
+//                 object.previous = Number(index) - 1
+//             }
+            
+//             // console.log(index, (query.i.length - 1))
+            
+//             if (index < (query.i.length - 1)) {
+//                 // Does this image need a next button, if it's the end of the carousel it does not need a next button.
+//                 object.next = Number(index) + 1
+//             }
 
-    if (typeof query.i === 'string') {
-        return [array[query.i]]
-    } else {
-        let filteredArray = []
-        query.i.forEach(index => {
-            filteredArray.push(array[index])
-        })
-        return filteredArray
-    }
-}
+//             filteredArray.push(object)
+//         })
+//         return filteredArray
+//     }
+// }
 
 router.get('/', (req, res) => {
     db.collection('images').find({}).toArray()
     .then(imageArray => {
-        let images
-        console.log(Object.keys(req.query).length === 0)
-
-        if ( Object.keys(req.query).length !== 0 ) {
-            // If there are any keys inside the query
-            images = filterImages(req.query, imageArray)
-        } else {
-            // If the query is empty
-            images = imageArray
-        }
-
-        console.log(images)
+        const images = imageArray
         return res.render('carousel', { template: 'carousel', images })
      })
+})
+
+router.post('/save', upload.none(), (req, res) => {
+    // Make sure to always save it as an array.
+    if (typeof req.body.saveSelection === 'string') {
+        req.body.saveSelection = [req.body.saveSelection]
+    }
+
+    // Write the saved setup to the server.
+    fs.writeFile('saved-carousel.json', JSON.stringify(req.body), 'utf8', () => {
+        console.log('Succesfully saved carousel.')
+    })
+
+    res.redirect('/carousel')
 })
 
 module.exports = router
