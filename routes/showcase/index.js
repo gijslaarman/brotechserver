c99f42f9a56b9cfeaa8dfe6d53cde5c98b68f3b7
gijslaarman <gijslaarman@live.nl>
2020-03-30 15:28:09 +0200

    better styling and more interactivity

--- routes/showcase/index.js
+++ routes/showcase/index.js
@@ -4,6 +4,20 @@ const router = express.Router({ mergeParams: true })
 router.get('/', (req, res) => {
     db.collection('images').find({checked: true}).toArray()
     .then(images => {
+        images = images.map((image, index) => {
+            if (index !== 0) {
+                image.previous = (index - 1).toString()
+            }
+
+            if (index !== (images.length - 1)) {
+                image.next = (index + 1).toString()
+            }
+
+            return image
+        })
+
+        console.log(images)
+
         res.render('showcase', { template: 'showcase', images })
     })
 })
